8513755b256e4e88f3fe61637c7e1cb6eff1343d
Ruben Incostante <rincostante@ambiente.gob.ar>
2016-02-05 19:39:30 -0300

    Primer commit luego de creada la aplicación a partir del arquetipo maven

---
+++ src/main/java/ar/gob/ambiente/aplicaciones/controlefluentesliquidos/mb/MbLogin.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ar.gob.ambiente.aplicaciones.controlefluentesliquidos.mb;
+
+import ar.gob.ambiente.aplicaciones.controlefluentesliquidos.entities.Usuario;
+import ar.gob.ambiente.aplicaciones.controlefluentesliquidos.facades.UsuarioFacade;
+import ar.gob.ambiente.aplicaciones.controlefluentesliquidos.util.CriptPass;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import org.omnifaces.util.Faces;
+
+/**
+ *
+ * @author rincostante
+ */
+public class MbLogin implements Serializable{
+
+    private boolean logeado = false;   
+    private String ambito;
+    private Usuario usLogeado;
+
+    @EJB
+    private UsuarioFacade usuarioFacade;
+    private boolean iniciado;
+    
+    /**
+     * Creates a new instance of MbLogin
+     */
+    public MbLogin() {
+    }
+    
+    /**
+     * Método para inicializar el listado de los Mb activos
+     */
+    @PostConstruct
+    public void init(){
+        iniciado = false;
+    }
+    
+    /**
+     * Método que borra de la memoria los MB innecesarios al cargar el listado 
+     */
+    public void iniciar() throws IOException{
+        if(iniciado){
+            String s;
+            HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
+            .getExternalContext().getSession(true);
+            Enumeration enume = session.getAttributeNames();
+            while(enume.hasMoreElements()){
+                s = (String)enume.nextElement();
+                if(s.substring(0, 2).equals("mb")){
+                    if(!s.equals("mbLogin")){
+                        session.removeAttribute(s);
+                    }
+                }
+            }
+        }else{
+            // obtengo el nombre del usuario logeado de la cookie correspondiente
+            String nomUsuario;
+            String valueEnc = Faces.getRequestCookie(ResourceBundle.getBundle("/Bundle").getString("nameCookieUser"));
+            
+            try {
+                // desencripto el nombre
+                nomUsuario = CriptPass.desencriptar(valueEnc);
+                
+                // obtebtengo el usuario correspondiente al nombre desencriptado
+                if(usuarioFacade.getUsuario(nomUsuario) != null){
+                    usLogeado = usuarioFacade.getUsuario(nomUsuario);
+                    ambito = usLogeado.getRol().getNombre();
+                    iniciado = true;
+                }else{
+                    // si no tengo un usuario registrado en la aplicación para este nombre, redirecciono a la vista de error de acceso
+                    FacesContext fc=FacesContext.getCurrentInstance();
+                    fc.getExternalContext().redirect(ResourceBundle.getBundle("/Bundle").getString("logError"));
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(MbLogin.class.getName()).log(Level.SEVERE, null, ex);
+            }            
+        }
+    }          
+
+    public boolean isLogeado() {
+        return logeado;
+    }
+
+    public void setLogeado(boolean logeado) {
+        this.logeado = logeado;
+    }
+
+    public String getAmbito() {
+        return ambito;
+    }
+
+    public void setAmbito(String ambito) {
+        this.ambito = ambito;
+    }
+
+    public Usuario getUsLogeado() {
+        return usLogeado;
+    }
+
+    public void setUsLogeado(Usuario usLogeado) {
+        this.usLogeado = usLogeado;
+    }
+
+    public boolean isIniciado() {
+        return iniciado;
+    }
+
+    public void setIniciado(boolean iniciado) {
+        this.iniciado = iniciado;
+    }
+    
+    public void logout(){
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+        session.invalidate();
+        iniciado = false;
+    }      
+}
